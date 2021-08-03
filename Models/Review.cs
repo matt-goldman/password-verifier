8d519bbb0b597d14fc68eb905f69a80b2a6b868a
Matt Goldman <matt.goldman@outlook.com>
2021-08-03 17:20:08 +1000

    adding reviews

---
+++ Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace password_verifier.Models
+{
+    public class Review
+    {
+        private readonly Random _random = new Random();
+        
+        public string ReviewerName { get; set; }
+        public string ReviewerLocation { get; set; }
+        public string ReviewerProfilePicUrl { get; set; }
+        public string ReviewText { get; set; }
+        public string ProductName { get; set; }
+        public int Rating { get; set; }
+
+        public DateTime PurchaseDate
+        {
+            get
+            {
+                var daysAgo = _random.Next(20, 120);
+                return DateTime.Now.AddDays(-daysAgo);
+            }
+        }
+    }
+}
---
+++ Services/ReviewsService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using password_verifier.Models;
+using password_verifier.Models.JsonHelpers;
+
+namespace password_verifier.Services
+{
+    public class ReviewsService
+    {
+        private readonly HttpClient _client;
+
+        private readonly Random _random = new Random();
+
+        private const string ApiUri = "https://randomuser.me/api/?results=4";
+
+        public ReviewsService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<Review>> GetReviews()
+        {
+            var profiles = await _client.GetFromJsonAsync<Result[]>(ApiUri);
+            
+            var reviewData = await _client.GetFromJsonAsync<ReviewDummyData[]>("sample-data/reviews.json");
+
+            List<Review> reviews = new List<Review>();
+
+            var profileReviews = reviewData?.OrderBy(r => _random.Next()).Take(4);
+
+            int i = 0;
+            
+            foreach (var profile in profiles)
+            {
+                reviews.Add(new Review()
+                {
+                    
+                });
+                i++;
+            }
+
+            return reviews;
+        }
+    }
+}
